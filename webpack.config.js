b5235f4e642aac26cec692b7011a56d00d910e5d
Marcel <mtrnord1@gmail.com>
2020-05-29 15:05:45 +0200

    Fix theme and fix hopefully webpack scss production
    Took 31 minutes

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const distPath = path.resolve(__dirname, "dist");
 module.exports = (env, argv) => {
@@ -21,8 +23,12 @@ module.exports = (env, argv) => {
             rules: [
                 {
                     test: /\.s[ac]ss$/i,
+                    include: [path.resolve(__dirname, "static"), path.resolve(__dirname, "node_modules")],
                     use: [
-                        'style-loader',
+                        // fallback to style-loader in development
+                        process.env.NODE_ENV !== 'production'
+                            ? 'style-loader'
+                            : MiniCssExtractPlugin.loader,
                         'css-loader',
                         'sass-loader',
                     ],
@@ -32,9 +38,9 @@ module.exports = (env, argv) => {
         plugins: [
             new CopyWebpackPlugin({
                 patterns: [
-                    {
+                    /*{
                         from: './static', to: distPath
-                    },
+                    },*/
                     {
                         from: './node_modules/uikit/dist/js/uikit.min.js',
                         to: path.join(distPath, '/js/uikit.min.js')
@@ -48,6 +54,13 @@ module.exports = (env, argv) => {
             new WasmPackPlugin({
                 crateDirectory: ".",
                 extraArgs: "--no-typescript",
+            }),
+            new MiniCssExtractPlugin({
+                filename: '[name].css',
+                chunkFilename: '[id].css',
+            }),
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, "static/index.html")
             })
         ],
         watch: argv.mode !== 'production'
